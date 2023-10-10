f7780b622a557f083d7285b70721c1a7664dd902
Your Name <you@example.com>
2023-10-10 18:21:50 +0300

    изменен логин

--- controllers/users.js
+++ controllers/users.js
@@ -87,7 +87,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res
         .cookie('jwt', token, {
-          maxAge: 7 * 24 * 60 * 60,
+          maxAge: 604800000,
           httpOnly: true,
           sameSite: true,
         })
